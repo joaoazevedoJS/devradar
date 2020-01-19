61263fed7205ea3153cbb320f5700ed90ce643dc
joaoazevedoJS <devjoaoazevedo@gmail.com>
2020-01-19 15:53:17 -0300

    Week10-day1

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const server = http.Server(app)
 
 setupWebsocket(server)
 
-mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-jdw02.mongodb.net/weel10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://<user>:<password>@<cluster>', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
